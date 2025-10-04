f7d42ec1b044b24a6aeb94b93b3bfc5472ba64f3
Alex <radosasha@gmail.com>
2025-10-04 17:22:21 +0300

    Refactoring

--- data/src/androidMain/kotlin/com/tracker/data/datasource/impl/AndroidTrackingDataSource.kt
+++ data/src/androidMain/kotlin/com/tracker/data/datasource/impl/AndroidTrackingDataSource.kt
@@ -1,57 +1,27 @@
 package com.tracker.data.datasource.impl
 
 import com.tracker.data.datasource.TrackingDataSource
-import com.tracker.data.mapper.LocationMapper
-import com.tracker.data.model.LocationDataModel
 import com.tracker.data.model.TrackingDataStatus
 import com.tracker.domain.datasource.TrackingRequester
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.map
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 /**
  * Android реализация TrackingDataSource
+ * Stateless - только делегирует вызовы в TrackingRequester
  */
 class AndroidTrackingDataSource : TrackingDataSource, KoinComponent {
-    
+
     private val trackingRequester: TrackingRequester by inject()
-    
-    private val _trackingStatusFlow = MutableSharedFlow<TrackingDataStatus>()
-    private var currentStatus = TrackingDataStatus.STOPPED
-    
+
     override suspend fun startTracking(): Result<Unit> {
-        return try {
-            val result = trackingRequester.startTracking()
-            if (result.isSuccess) {
-                currentStatus = TrackingDataStatus.ACTIVE
-                _trackingStatusFlow.emit(currentStatus)
-            }
-            result
-        } catch (e: Exception) {
-            currentStatus = TrackingDataStatus.ERROR
-            _trackingStatusFlow.emit(currentStatus)
-            Result.failure(e)
-        }
+        return trackingRequester.startTracking()
     }
-    
+
     override suspend fun stopTracking(): Result<Unit> {
-        return try {
-            val result = trackingRequester.stopTracking()
-            if (result.isSuccess) {
-                currentStatus = TrackingDataStatus.STOPPED
-                _trackingStatusFlow.emit(currentStatus)
-            }
-            result
-        } catch (e: Exception) {
-            currentStatus = TrackingDataStatus.ERROR
-            _trackingStatusFlow.emit(currentStatus)
-            Result.failure(e)
-        }
+        return trackingRequester.stopTracking()
     }
-    
+
     override suspend fun getTrackingStatus(): TrackingDataStatus {
         return if (trackingRequester.isTrackingActive()) {
             TrackingDataStatus.ACTIVE
--- data/src/iosMain/kotlin/com/tracker/data/datasource/impl/IOSTrackingDataSource.kt
+++ data/src/iosMain/kotlin/com/tracker/data/datasource/impl/IOSTrackingDataSource.kt
@@ -3,50 +3,25 @@ package com.tracker.data.datasource.impl
 import com.tracker.data.datasource.TrackingDataSource
 import com.tracker.data.model.TrackingDataStatus
 import com.tracker.domain.datasource.TrackingRequester
-import kotlinx.coroutines.flow.MutableSharedFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 /**
  * iOS реализация TrackingDataSource
+ * Stateless - только делегирует вызовы в TrackingRequester
  */
 class IOSTrackingDataSource : TrackingDataSource, KoinComponent {
-    
+
     private val trackingRequester: TrackingRequester by inject()
-    
-    private val _trackingStatusFlow = MutableSharedFlow<TrackingDataStatus>()
-    private var currentStatus = TrackingDataStatus.STOPPED
-    
+
     override suspend fun startTracking(): Result<Unit> {
-        return try {
-            val result = trackingRequester.startTracking()
-            if (result.isSuccess) {
-                currentStatus = TrackingDataStatus.ACTIVE
-                _trackingStatusFlow.emit(currentStatus)
-            }
-            result
-        } catch (e: Exception) {
-            currentStatus = TrackingDataStatus.ERROR
-            _trackingStatusFlow.emit(currentStatus)
-            Result.failure(e)
-        }
+        return trackingRequester.startTracking()
     }
-    
+
     override suspend fun stopTracking(): Result<Unit> {
-        return try {
-            val result = trackingRequester.stopTracking()
-            if (result.isSuccess) {
-                currentStatus = TrackingDataStatus.STOPPED
-                _trackingStatusFlow.emit(currentStatus)
-            }
-            result
-        } catch (e: Exception) {
-            currentStatus = TrackingDataStatus.ERROR
-            _trackingStatusFlow.emit(currentStatus)
-            Result.failure(e)
-        }
+        return trackingRequester.stopTracking()
     }
-    
+
     override suspend fun getTrackingStatus(): TrackingDataStatus {
         return if (trackingRequester.isTrackingActive()) {
             TrackingDataStatus.ACTIVE
