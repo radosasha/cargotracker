f3391ed01c239b8a25d610ad7abc8489e7155a49
Alex <radosasha@gmail.com>
2025-10-17 00:04:37 +0300

    Add ktlint rules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,33 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
 }
+
+// Общая конфигурация ktlint для всех модулей
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.2.1")
+        android.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(false)
+        
+        filter {
+            exclude("**/generated/**")
+            exclude("**/build/**")
+            include("**/kotlin/**")
+        }
+        
+        // Отключить проблемные правила (новый синтаксис)
+        disabledRules.set(listOf(
+            "function-naming",
+            "class-naming",
+            "discouraged-comment-location",
+            "no-empty-first-line-in-class-body",
+            "blank-line-before-declaration",
+            "wrapping",
+            "parameter-list-wrapping",
+            "multiline-expression-wrapping"
+        ))
+    }
+}
